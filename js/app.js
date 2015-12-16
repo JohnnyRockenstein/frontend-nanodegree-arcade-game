bb4f345d52a5967b92e111929582b85b35223b50
Johnnyrockenstein <dustinhorrock@gmail.com>
2015-12-15 20:21:21 -0600

    Cleaned up and added comments

--- js/app.js
+++ js/app.js
@@ -1,27 +1,16 @@
-// Enemies our player must avoid
 var Enemy = function(x, y, velocity) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
     this.x = x;
     this.y = y;
     this.velocity = velocity;
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
     this.x += this.velocity * dt;
     if (this.x >= 500) {
         this.x = -50;
     };
     checkObjectCollision(player, this);
-
 };
 
 // Draw the enemy on the screen, required method for game
@@ -33,30 +22,22 @@ Enemy.prototype.render = function() {
 // This class requires an update(), render() and
 // a handleInput() method.
 
-var Player = function(x, y, velocity){
+var Player = function(x, y, velocity) {
     this.x = x;
     this.y = y;
     this.velocity = velocity;
     this.sprite = 'images/char-boy.png';
 };
-    Player.prototype.update = function(){
-        if (player.y + 63 <= 44 ){
+    Player.prototype.update = function() {
+        if (player.y + 63 <= 44) {
             scorepoint();
         }
         this.x = bindObjectToBounds(this.x, 400, 10);
         this.y = bindObjectToBounds(this.y, 380, 0);
-
-
-
-};
-
-
-
-    Player.prototype.render = function(){
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-
     };
-
+    Player.prototype.render = function() {
+        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    };
     Player.prototype.handleInput = function(keypress){
         if (keypress == 'left'){
             player.x -= player.velocity;
@@ -70,63 +51,59 @@ var Player = function(x, y, velocity){
         if (keypress == 'right'){
             player.x += player.velocity;
         }
-
+    };
+    Player.prototype.resetPosition = function() {
+        this.x = 200;
+        this.y = 380;
     };
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
-
-
-var levelUp = function(){
-    allEnemies.length  = 0;
+// Increases the number of enemies in the stage
+var levelUp = function() {
+    allEnemies.length = 0;
     for (var i = 0; i <= level; i++) {
         var enemy = new Enemy(Math.random() * 400, Math.random() * 184 + 50, Math.random() * 300);
-
         allEnemies.push(enemy);
     };
     level++;
+    console.log("Advanced to level " + level);
     return level;
-    console.log(level);
 };
 
-
-var scorepoint = function(){
-    player.x = 200;
-    player.y = 380;
+// Awards player a point for reaching water and returns him to start location
+var scorepoint = function() {
+    ctx.fillStyle = 'white';
+    ctx.fillRect(0, 0, 505, 171);
     levelUp();
+    player.resetPosition();
 };
 
-var bindObjectToBounds = function(object, max, min){
-    
-    if (object <= min){
+// Sets movement boundary to an object
+var bindObjectToBounds = function(object, max, min) {
+    if (object <= min) {
         return min;
-        }
-        else if (object >= max){
-            return max;
-        };
-        return object;
-}
+    } else if (object >= max) {
+        return max;
+    };
+    return object;
+};
 
-var checkObjectCollision = function(firstobject, secondobject){
+// Checks if an object collides with another object
+var checkObjectCollision = function(firstobject, secondobject) {
      if (
         firstobject.y + 139 >= secondobject.y + 90
         && firstobject.x + 18 <= secondobject.x + 88
         && firstobject.y + 100 <= secondobject.y + 135
-        && firstobject.x + 84 >= secondobject.x + 11) {
-        console.log('REKT');
-        firstobject.x = 202.5;
-        firstobject.y = 383;
-
-}
+        && firstobject.x + 84 >= secondobject.x + 11){
+            console.log('You collided with an object');
+            firstobject.resetPosition();
+        };
 };
+
+//Initialize Player and Enemies
 var player = new Player(200, 380, 50);
 var allEnemies = [];
 var level = 0;
 levelUp();
-
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
